b5698b40e6af80dee57d9f8bfd139df741b47bd9
Olman <olmansr2007@gmail.com>
2025-06-12 19:13:14 -0600

    nuevo cambio

--- src/calculadora_funciones_trigonometricas/Calculadora_funciones_trigonometricas.java
+++ src/calculadora_funciones_trigonometricas/Calculadora_funciones_trigonometricas.java
@@ -1,20 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package calculadora_funciones_trigonometricas;
-
-/**
- *
- * @author olman
- */
-public class Calculadora_funciones_trigonometricas {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
-}
+package calculador_trigonometrico;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader    

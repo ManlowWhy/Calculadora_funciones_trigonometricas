e3bd91f53a5b89aabea94615c4800b9e290bab4f
ManlowWhy <olmansr2007@gmail.com>
2025-06-12 19:19:37 -0600

    Agrego la clase

--- src/calculadora_funciones_trigonometricas/Calculadora_funciones_trigonometricas.java
+++ src/calculadora_funciones_trigonometricas/Calculadora_funciones_trigonometricas.java
@@ -1,4 +1,5 @@
 package calculador_trigonometrico;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader    
+import java.io.InputStreamReader   
+public class Calculador_trigonometrico {

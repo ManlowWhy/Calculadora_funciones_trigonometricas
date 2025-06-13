f7030f651e41991bb6672a2935e9a1f7b9c7289d
EmersonWallace <enowallace2007@gmail.com>
2025-06-12 19:30:41 -0600

    Update Calculadora_funciones_trigonometricas.java

--- src/calculadora_funciones_trigonometricas/Calculadora_funciones_trigonometricas.java
+++ src/calculadora_funciones_trigonometricas/Calculadora_funciones_trigonometricas.java
@@ -2,4 +2,27 @@ package calculador_trigonometrico;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader   
+  
 public class Calculador_trigonometrico {
+  
+    public static void main(String[] args) {
+             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        boolean salir = false;
+
+        while (!salir) {
+            // Mostrar menú
+            System.out.println(" Calculadora Trigonometrica ");
+            System.out.println("1. sin(α)");
+            System.out.println("2. cos(α)");
+            System.out.println("3. tan(α)");
+            System.out.println("4. arcsin(α)");
+            System.out.println("5. arccos(α)");
+            System.out.println("6. arctan(α)");
+            System.out.println("7. sinh(α)");
+            System.out.println("8. cosh(α)");
+            System.out.println("9. tanh(α)");
+            System.out.println("0. Salir");
+            System.out.print("Elige una opcion: ");
+
+            String lineaOpcion;
+            int opcion = -1;
